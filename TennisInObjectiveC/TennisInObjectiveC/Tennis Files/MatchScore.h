989acd3c1762901e17a7692e0e445b6fdeeffc33
Stephen Kyles <BlueOwl@Blues-MacBook-Pro.local>
2014-01-31 23:11:08 -0800

    No tiebreaker, also not printing sets correctly

--- TennisInObjectiveC/TennisInObjectiveC/Tennis Files/MatchScore.h
+++ TennisInObjectiveC/TennisInObjectiveC/Tennis Files/MatchScore.h
@@ -7,8 +7,13 @@
 //
 
 #import "Score.h"
+#import "SetScore.h"
 
 @interface MatchScore : Score
+{
+    int setNumber;
+    SetScore *scores[5];
+}
 
 -(instancetype) initWithFirstPlayer: (Player *) p1 secondPlayer: (Player *) p2;
 
--- TennisInObjectiveC/TennisInObjectiveC/Tennis Files/MatchScore.m
+++ TennisInObjectiveC/TennisInObjectiveC/Tennis Files/MatchScore.m
@@ -22,8 +22,21 @@
     return self.player1Score == 3 || self.player2Score == 3;
 }
 
+/*
+-(void)addScore:(SetScore *)score
+{
+    //scores[setNumber] = (SetScore *)score;
+    //setNumber++;
+    //[score getWinner] == self.player1 ? self.player1Score++ : self.player2Score++;
+}
+*/
+
 -(NSString *) description
 {
+    NSLog(@"Set No.    Player A          Player B\n");
+    for( int i = 0; i < setNumber; i++ ) {
+        NSLog(@"%@", scores[i]);
+    }
     return self.player1Score > self.player2Score ?
     [NSString stringWithFormat:@"\n\nplayer 1 wins %d to %d\n\n", self.player1Score, self.player2Score] :
     [NSString stringWithFormat:@"\n\nplayer 2 wins %d to %d\n\n", self.player2Score, self.player1Score];
